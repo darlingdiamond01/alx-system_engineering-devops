15c57bbc7e474e6f20731c1a561e69adee17a2a0
darlingdiamond01 <darlingmaria01@gmail.com>
2024-03-22 14:46:12 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install flask from pip3 using Puppet
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }

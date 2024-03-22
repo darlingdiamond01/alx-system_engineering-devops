8eea39ef28a6614601bf612b1179049b199fb550
darlingdiamond01 <darlingmaria01@gmail.com>
2024-03-22 06:13:45 -0700

    configuration manangement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-package { 'python3-pip':
-  ensure => installed,
-}
+# Using Puppet, install flask from pip3.
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

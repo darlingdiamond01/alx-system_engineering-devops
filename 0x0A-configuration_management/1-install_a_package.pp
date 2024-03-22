21e16e98dc8d43dfba2a444a0bea9bc442b839d5
darlingdiamond01 <darlingmaria01@gmail.com>
2024-03-22 06:08:39 -0700

    configuration manangement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-#!/usr/bin/pup
+# install puppet-lint -v 2.1.0
 
-# Install a specific version of flask (2.1.0)
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
